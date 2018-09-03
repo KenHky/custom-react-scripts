774979d7695217140aa469337975510c18b0bc12
Ken Huang <ken.hky@163.com>
2018-09-03 16:06:22 +0800

    feat(package): rename

--- packages/react-scripts/config/custom-react-scripts/options/extract-text-plugin-options.js
+++ packages/react-scripts/config/custom-react-scripts/options/extract-text-plugin-options.js
@@ -1,7 +1,7 @@
 const paths = require('../../paths');
 
 const publicPath = paths.servedPath;
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+const cssFilename = 'static/css/[name].css';
 const shouldUseRelativeAssetPaths = publicPath === './';
 
 module.exports = shouldUseRelativeAssetPaths
